35bca68dde057f720140272ea615afe42317f4a0
Chris-Duignan <cduignan.1993@gmail.com>
2023-09-28 19:34:04 +0100

    feat: add initial setup

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,18 @@
+const AboutMe = () => {
+  return (
+    <div>
+        <h2>About Me</h2>
+      <p>
+        Hi, I'm Chris, Software/ Data Engineer, and board game enthusiast. I
+        began my switch into tech in August 2023, completing the Northcoders
+        Software Engineering bootcamp. After finishing the course, I moved into
+        a role with Northcoders and worked with a client as a frontend software
+        engineer. After finishing work with the client, I moved into learning
+        the fundamentals of data engineering, and cloud infrastructure (AWS,
+        Microsoft Azure).
+      </p>
+    </div>
+  );
+};
+
+export default AboutMe;
---
+++ src/components/TechStack.tsx
@@ -0,0 +1,9 @@
+const TechStack = () => {
+    return (
+        <div>
+            <h2>Tech Stack</h2>
+        </div>
+    )
+}
+
+export default TechStack;
