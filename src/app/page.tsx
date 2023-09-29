deefd5ad1a89510f691b1bd2a824979ad4806fc7
Chris-Duignan <cduignan.1993@gmail.com>
2023-09-29 21:33:16 +0100

    feat: add skeleton;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import AboutMe from '@/components/AboutMe';
 import './globals.css'
 import Banner from "@/components/Banner";
 import TechStack from '@/components/TechStack';
+import Projects from '@/components/Projects';
+import ContactMe from '@/components/ContactMe';
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       <Banner></Banner>
       <AboutMe></AboutMe>
       <TechStack></TechStack>
+      <Projects></Projects>
+      <ContactMe></ContactMe>
     </main>
   )
 }
