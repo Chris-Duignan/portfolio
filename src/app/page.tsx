7007c228273314fd9d52089d6b90d90b003189dd
Chris-Duignan <cduignan.1993@gmail.com>
2023-10-04 13:31:31 +0100

    feat: add fixed sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 import "./globals.css";
-import styles from "./page.module.css";
 import AboutMe from "../components/AboutMe";
-import TechStack from "../components/TechStack";
-import Projects from "../components/Projects";
-import ContactMe from "../components/ContactMe";
+import Container from "@/components/Container";
+import NavBar from "@/components/NavBar";
+import TechStack from "@/components/TechStack";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
-    <>
-      <main className={styles.main}>
-        <AboutMe></AboutMe>
-        <TechStack></TechStack>
-        <Projects></Projects>
-      </main>
-    </>
+    <div className="grid grid-cols-3 gap-4 h-screen">
+      <div className="col-span-1 border-r-2 pl-60 pt-44">
+        <div>
+          <h1 className="text-3xl">Chris Duignan</h1>
+          <h2 className="text-xl">Full Stack Software Engineer</h2>
+        </div>
+        <NavBar />
+      </div>
+      <div className='col-span-2 text-justify overflow-y-auto py-44 pr-60 pl-16'>
+        <AboutMe />
+        <TechStack />
+        <Projects />
+      </div>
+    </div>
   );
 }
