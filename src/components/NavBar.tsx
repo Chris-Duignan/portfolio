7f29fe0e73d443fa87737b3ecb42a6ed3b079c29
Chris-Duignan <cduignan.1993@gmail.com>
2023-10-04 13:35:41 +0100

    feat: add initial navbar setup

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav className="pt-[75px]">
+            <ul>
+                <li className="pb-[10px]">About Me</li>
+                <li className="pb-[10px]">Tech Stack</li>
+                <li className="pb-[10px]">Projects</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
