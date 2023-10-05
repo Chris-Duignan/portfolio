f49d87e412c7417008d512f2ba736557bebe4f99
Chris-Duignan <cduignan.1993@gmail.com>
2023-10-05 21:36:40 +0100

    feat: add styling to highlight links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,17 @@
-const NavBar = () => {
+interface Props {
+    section: string
+}
+
+const NavBar = ({ section }: Props) => {
+
+    const style = "pb-6"
+    const newStyle = "pb-6 font-bold underline"
     return (
         <nav className="pt-[75px]">
             <ul>
-                <li className="pb-[10px]">About Me</li>
-                <li className="pb-[10px]">Tech Stack</li>
-                <li className="pb-[10px]">Projects</li>
+                <li className={section==="bio" ? newStyle: style}><a href="#bio">About Me</a></li>
+                <li className={section==="tech" ? newStyle: style}><a href="#tech">Tech Stack</a></li>
+                <li className={section==="projects" ? newStyle: style}><a href="#projects">Projects</a></li>
             </ul>
         </nav>
     )
