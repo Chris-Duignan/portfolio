c4c3d1e6aa7706f2c359eec8a09d3bc6777097a8
Chris-Duignan <cduignan.1993@gmail.com>
2023-10-09 16:36:08 +0100

    feat: add infinite scrolling for project cards

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,17 +1,37 @@
 import type { Record } from "@/interfaces/types";
 import Image from "next/image";
+import React from "react";
+import { useEffect, useRef } from "react";
 
 interface ProjectCardProps {
   record: Record;
+  isLast: boolean;
+  loadMoreProjects: () => void
 
 }
 
-const ProjectCard = (({record}: ProjectCardProps) => {  
+const ProjectCard = (({record, isLast, loadMoreProjects}: ProjectCardProps) => {  
+
+  const cardRef = React.createRef<HTMLDivElement>()
+  const loadMoreTriggered = useRef(false);
+
+  useEffect(() => {
+    if (!cardRef.current) return;
+    const observer = new IntersectionObserver(([entry]) => {
+      if (isLast && entry.isIntersecting && !loadMoreTriggered.current) {
+        loadMoreTriggered.current = true;
+        loadMoreProjects()
+        observer.unobserve(entry.target)
+      }
+    }, {threshold: 1})
+
+    observer.observe(cardRef.current)
+  }, [isLast])
 
   return (
-    <div className="p-10 card">
+    <div className="p-10 card" ref={cardRef}>
       <h3>{record.fields.name}</h3>
-      <Image
+      {(record.fields.images) ? <Image
         src={record.fields.images[0].thumbnails.large.url}
         alt=""
         height={record.fields.images[0].thumbnails.large.height}
@@ -22,18 +42,19 @@ const ProjectCard = (({record}: ProjectCardProps) => {
         }}
         placeholder="blur"
         blurDataURL="/1x1-b92e297f.png"
-      />
+      /> : null}
       <p>{record.fields.description}</p>
-      <p>
+      {(record.fields.hosted) ? <p>
         See it{" "}
         <a href={record.fields.hosted} target="_blank">
           here
-        </a>{" "}
-        and on{" "}
+        </a>{" "}</p>: null}
+
+      {(record.fields.github) ? <p>and on{" "}
         <a href={record.fields.github} target="_blank">
           GitHub
         </a>
-      </p>
+      </p>: null}
     </div>
   );
 });
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,46 +1,39 @@
-import React, { Suspense, useEffect, useRef } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import useProjects from "@/hooks/useProjects";
 import ProjectCard from "./ProjectCard";
 
 const Projects = React.forwardRef<HTMLDivElement>((props, ref) => {
-  const { currentProjects, allCollected, setNextPage, err } = useProjects();
+  const { projects, err } = useProjects();
+  const [page, setPage] = useState(1);
+  const [currentProjects, setCurrentProjects] = useState(
+    projects.slice(0, page * 2)
+  );
 
   useEffect(() => {
+    setCurrentProjects(projects.slice(0, page * 2));
+  }, [projects, page]);
 
-    if (!allCollected) {
-      const observer = new IntersectionObserver((entries) => {
-        const lastCard = entries[0]
-  
-        if (!lastCard.isIntersecting) return;
-        setNextPage()
-        observer.unobserve(lastCard.target)
-        if (!allCollected) observer.observe(document.querySelector('.card:last-child')!)
-      }, {threshold: 1});
-  
-      if (document.querySelector('.card:last-child')) observer.observe(document.querySelector('.card:last-child')!)
-      return () => {
-        observer.disconnect()
-      }
-    }
-
-  }, [setNextPage, allCollected]);
+  const loadMoreProjects = () => {
+    setPage((prev) => {
+      return prev + 1;
+    });
+  };
 
   return (
     <div id="projects" ref={ref}>
       {err ? <div>oh no</div> : null}
-      {currentProjects.map((data) => {
-        return (
-          <Suspense
-            key={data.id}
-            fallback={<div className="text-gray-50">Loading...</div>}
-          >
+      <Suspense>
+        {currentProjects.map((data, index) => {
+          return (
             <ProjectCard
               record={data}
               key={data.id}
+              isLast={index === currentProjects.length - 1}
+              loadMoreProjects={loadMoreProjects}
             />
-          </Suspense>
-        );
-      })}
+          );
+        })}
+      </Suspense>
     </div>
   );
 });
