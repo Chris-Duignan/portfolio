855fc2b1a6dfb5c7bd7c8fc249f19255661a9521
Chris-Duignan <cduignan.1993@gmail.com>
2023-11-10 18:08:29 +0000

    disable caching

--- src/app/api/project/route.ts
+++ src/app/api/project/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
 
-  const {body} = request;
+  // const {body} = request;
 
   try {
     const res = await fetch(
