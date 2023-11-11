1e32df36e534a1c21c2f2355dd3936629647a0d8
Chris-Duignan <cduignan.1993@gmail.com>
2023-11-11 17:58:05 +0000

    test airtable sdk

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,8 +1,23 @@
 import { NextResponse } from "next/server";
+import Airtable from "airtable";
 
 
 export async function GET(request: Request) {
     const { body } = request
 
-    return NextResponse.json({message: 'Hello world'}, {status: 200})
+    try {
+        if (!process.env.AIRTABLE_BASE) throw new Error('Base missing')
+        const base = new Airtable({apiKey: process.env.AIRTABLE_TOKEN}).base(process.env.AIRTABLE_BASE)
+        
+    const records = await base('Projects').select({}).firstPage()
+
+    const minifiedRecords = records.map((record) => {
+        return record.fields
+    })
+
+    return NextResponse.json(minifiedRecords, {status: 200})
+    } catch(err: any) {
+        return NextResponse.json({message: err.message}, {status: err.status})
+    }
+
 }
