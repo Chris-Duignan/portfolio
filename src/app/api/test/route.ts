f1a8880e0df99d4d8154bf577f113c1c7fc5d482
Chris-Duignan <cduignan.1993@gmail.com>
2023-11-11 18:08:18 +0000

    test fetch

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -6,16 +6,20 @@ export async function GET(request: Request) {
     const { body } = request
 
     try {
-        if (!process.env.AIRTABLE_BASE) throw new Error('Base missing')
-        const base = new Airtable({apiKey: process.env.AIRTABLE_TOKEN}).base(process.env.AIRTABLE_BASE)
+    //     if (!process.env.AIRTABLE_BASE) throw new Error('Base missing')
+    //     const base = new Airtable({apiKey: process.env.AIRTABLE_TOKEN}).base(process.env.AIRTABLE_BASE)
         
-    const records = await base('Projects').select({}).firstPage()
+    // const records = await base('Projects').select({}).firstPage()
 
-    const minifiedRecords = records.map((record) => {
-        return record.fields
-    })
+    // const minifiedRecords = records.map((record) => {
+    //     return record.fields
+    // })
+    const records = await fetch(`https://api.airtable.com/v0/${process.env.AIRTABLE_BASE}/projects`, {
+        headers: { Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}` }    })
 
-    return NextResponse.json(minifiedRecords, {status: 200})
+        const data = await records.json()
+
+    return NextResponse.json(data, {status: 200})
     } catch(err: any) {
         return NextResponse.json({message: err.message}, {status: err.status})
     }
